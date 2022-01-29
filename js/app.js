060e535509038301e1b99bc2acbc50a1d4c40600
叶凌一 <2864285801@qq.com>
2022-01-29 15:17:18 +0800

    Site updated: 2022-01-29 15:17:16

--- js/app.js
+++ js/app.js
@@ -1,33 +1,21 @@
-// 表格相册
-$("table")
-  .has("img")
-  .addClass("nexmoe-album");
-
-// 搜索
-function search() {
-  window.open($("#search_form").attr("action_e") + " " + $("#search_value").val());
-  return false;
+// 设置 tabs 标签
+function setTabs() {
+  $(".tabs .nav-tabs").on("click", "a", (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    const $tab = $(e.target.parentElement.parentElement.parentElement);
+    $tab.find(".nav-tabs .active").removeClass("active");
+    $tab.find(e.target.parentElement).addClass("active");
+    $tab.find(".tab-content .active").removeClass("active");
+    $tab.find($(e.target).attr("class")).addClass("active");
+    return false;
+  });
 }
 
-// 平滑跳转同时修复锚点链接被转义
-$(document).ready(function () {
-  $("a.toc-link").click(function (ev) {
-    ev.preventDefault();
-    $("html, body").animate({
-      scrollTop: $(decodeURI($(this).attr("href"))).offset().top - 25
-    }, {
-      duration: 500,
-      easing: "swing"
-    });
-  });
+$(function () {
+  setTabs();
 });
 
-$(document).on("copy", function(){
-  if(!window.copyTip){ return; }
-  var sel = document.getSelection();
-  var ele = document.createElement("div");
-  ele.innerHTML = '<div style="position: fixed;opacity: 0;white-space: pre;">' + sel + "\n\n" + window.copyTip.replaceAll("%url",document.location.href) + ' </div>' 
-  document.body.appendChild(ele);
-  sel.selectAllChildren(ele);
-  setTimeout(function () {document.body.removeChild(ele);});
+document.addEventListener('pjax:complete', function () {
+  setTabs();
 });
